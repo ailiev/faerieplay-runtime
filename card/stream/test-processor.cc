161e191b92a3e943056e91de8911c40a31927f24
Alex Iliev <alex.iliev@gmail.com>
2011-12-24 15:38:52 +0000

    Updated Iliev email.

--- card/stream/test-processor.cc
+++ card/stream/test-processor.cc
@@ -2,7 +2,7 @@
  * Circuit virtual machine for the Faerieplay hardware-assisted secure
  * computation project at Dartmouth College.
  *
- * Copyright (C) 2003-2007, Alexander Iliev <sasho@cs.dartmouth.edu> and
+ * Copyright (C) 2003-2007, Alexander Iliev <alex.iliev@gmail.com> and
  * Sean W. Smith <sws@cs.dartmouth.edu>
  *
  * All rights reserved.
